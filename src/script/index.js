4206aba4a587446e2fbead195d197301ddc2603f
gabi-lima <gabriel1277lima@gmail.com>
2022-12-09 00:03:55 -0300

    Funcionalidades Iniciais feitas!
    Após quebrar um pouco a cabeça, as funcionalidades iniciais estão funcionando.
    Possível adicionar gastos e faturamentos.
    O algoritmo faz o cálculo assim que adicionado o valor, em seguida mostra o total liquido.
    Antes de dar um toque visual, pretendo adicionar um banco de dados, e também  mais funcionalidades, como alterar a lista, modificar o nome e remover o elemento da lista.

--- src/script/index.js
+++ src/script/index.js
@@ -6,29 +6,54 @@ function adicionarGastos(){
     var valorGastos = document.getElementById('valorGastos').value
     gastos.push(valorGastos)
     var matrizDeNumeros = gastos.map(Number);
-    const totalgastos = matrizDeNumeros.reduce((totalgastos, currentElement) => totalgastos + currentElement)
-    console.log(gastos)
-    console.log(totalgastos)
+    totalgastos = matrizDeNumeros.reduce((totalgastos, currentElement) => totalgastos + currentElement)
+    gastosCalc = totalgastos
+    /*console.log(gastos)
+    console.log(totalgastos)*/
 
     addLista = document.createElement('li');
     addLista.innerHTML = nomeGastos + ':   ' + valorGastos;
 
     document.getElementById('listaGastos').appendChild(addLista);
 
+    var contGastos = document.getElementById('totalGastos')
+    contGastos.innerHTML = "Total de gastos:  R$" + totalgastos
     /*console.log(nomeGastos)
     console.log(valorGastos) */
+    return gastosCalc
 }
+
 function adicionarRecebidos(){
     var nomeGanho = document.getElementById('nomeGanho').value
     var valorGanho = document.getElementById('valorGanho').value
+    ganhos.push(valorGanho)
+    var matrizGanhos = ganhos.map(Number);
+    totalGanhos = matrizGanhos.reduce((totalGanhos, currentElement) => totalGanhos + currentElement)
+    ganhosCalc = totalGanhos
     
     addLista = document.createElement('li');
     addLista.innerHTML = nomeGanho + ':   ' + valorGanho;
 
     document.getElementById('listaGanhos').appendChild(addLista);
+    var contGanhos = document.getElementById('totalGanhos')
+    contGanhos.innerHTML = "Total de gastos:  R$" + totalGanhos
 
-    console.log(valorGanho)
-    console.log(nomeGanho)
+    /*console.log(ganhos)
+    console.log(totalGanhos) */
+
+    return ganhosCalc
+    /*console.log(valorGanho)
+    console.log(nomeGanho) */
 
 }
+
+function totalLiquido(){
+    console.log(ganhosCalc - gastosCalc)
+    total = ganhosCalc - gastosCalc
+    var addLiquido = document.getElementById('totalLiq')
+    addLiquido.innerHTML = "Total Líquido: R$" + total 
+    console.log(total) 
+}
+
+
 
