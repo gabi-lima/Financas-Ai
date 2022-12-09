6ecd1fa6c66c44c2c13b1060665b614b03d4984c
gabi-lima <gabriel1277lima@gmail.com>
2022-12-08 21:51:53 -0300

    Adicionada a funcionalidade de soma
    Atualmente somente a soma entre os gastos está escrita em código, mas devo utilizar o mesmo código para os ganhos. Pensando também, em posteriormente fazer uma refatoração, e criar uma função que seja chamada para os dois, para 'limpar o código'.

--- src/script/index.js
+++ src/script/index.js
@@ -1,12 +1,34 @@
+ganhos = []
+gastos = []
+
 function adicionarGastos(){
     var nomeGastos = document.getElementById('nomeGastos').value
     var valorGastos = document.getElementById('valorGastos').value
-    
+    gastos.push(valorGastos)
+    var matrizDeNumeros = gastos.map(Number);
+    const totalgastos = matrizDeNumeros.reduce((totalgastos, currentElement) => totalgastos + currentElement)
+    console.log(gastos)
+    console.log(totalgastos)
+
     addLista = document.createElement('li');
-    addLista.innerHTML = nomeGastos + '   :   ' + valorGastos;
+    addLista.innerHTML = nomeGastos + ':   ' + valorGastos;
 
     document.getElementById('listaGastos').appendChild(addLista);
-    console.log(nomeGastos)
-    
-    console.log(valorGastos)
+
+    /*console.log(nomeGastos)
+    console.log(valorGastos) */
 }
+function adicionarRecebidos(){
+    var nomeGanho = document.getElementById('nomeGanho').value
+    var valorGanho = document.getElementById('valorGanho').value
+    
+    addLista = document.createElement('li');
+    addLista.innerHTML = nomeGanho + ':   ' + valorGanho;
+
+    document.getElementById('listaGanhos').appendChild(addLista);
+
+    console.log(valorGanho)
+    console.log(nomeGanho)
+
+}
+
