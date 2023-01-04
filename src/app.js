2dfec011c4dd99131167fa3a7e203ef6fb55283b
Gabriel Lima <gabriel1277lima@gmail.com>
2023-01-04 15:01:05 -0300

    Adicionado node, express e ejs no projetto

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const path = require("path");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
-app.use(express.static(__dirname + "/public"));
-app.get("/"),
-  (req, res, next) => {
-    res.send("<h1>S</h1>");
-  };
+
+app.use(express.static(path.join(__dirname + "/public/")));
+
+app.get("/", function (req, res, next) {
+  res.render("index");
+});
 
 app.listen(3000);
