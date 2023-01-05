ce798e0f4ceff755da7bd5ffc69a69d8269d7f14
Gabriel Lima <gabriel1277lima@gmail.com>
2023-01-04 23:45:25 -0300

    Reformulando o site visualemnte, estilo da nav definido

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname + "/public/")));
 
 app.get("/", function (req, res, next) {
