672bc6203d399fe4f66cefd125c664c8f1648fc9
Gabriel Lima <gabriel1277lima@gmail.com>
2023-01-05 03:59:57 -0300

    corrigido botão hamburguer e botão close

--- src/public/js/nav.js
+++ src/public/js/nav.js
@@ -1,6 +1,16 @@
 const btn_mobile = document.querySelector("#side-menu-toggle");
 
-btn_mobile.addEventListener("click", () => {
-  const nav = document.querySelector("#nav");
+const nav = document.querySelector("#nav");
+
+const btn = document.querySelector(".material-symbols-outlined");
+function listenToClick() {
   nav.classList.toggle("active");
-});
+  return changeBurger();
+}
+function changeBurger() {
+  if (nav.classList.contains("active")) {
+    btn.innerHTML = "close";
+  } else btn.innerHTML = "menu";
+}
+console.log(nav.classList.contains("active"));
+btn_mobile.addEventListener("click", listenToClick);
