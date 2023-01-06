e92481e8ca1dca3f4169618dc79f62aa9ec30256
Gabriel Lima <gabriel1277lima@gmail.com>
2023-01-06 06:53:30 -0300

    adicionanando elementos ao index

--- src/app.js
+++ src/app.js
@@ -9,8 +9,20 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname + "/public/")));
 
-app.get("/", function (req, res, next) {
+app.get("/", (req, res, next) => {
   res.render("index");
+});
+
+app.get("/financas", (req, res, next) => {
+  res.render("main-hub");
+});
+
+app.get("/sobre", (req, res, next) => {
+  res.render("about");
+});
+
+app.get("/login", () => {
+  res.render("login");
 });
 
 app.listen(3000);
