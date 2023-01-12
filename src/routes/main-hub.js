464ccbd31d86cae2ae9baa9485059ea94403d6f1
Gabriel Lima <gabriel1277lima@gmail.com>
2023-01-12 12:00:15 -0300

    Rota para /financas criada

--- src/routes/main-hub.js
+++ src/routes/main-hub.js
@@ -1,17 +1,30 @@
-const path = require("path");
 const express = require("express");
 const router = express.Router();
 const tabelaGasto = [];
+const tabelaGanho = [];
 
-router.get("/financas", (req, res, next) => {
+router.get("/", (req, res, next) => {
   res.render("main-hub");
 });
-
 router.post("/add_gasto", (req, res, next) => {
-  tabelaGasto.push({ nome_gasto: req.body.nome_gasto });
+  tabelaGasto.push({
+    nome: req.body.nome,
+    valor: req.body.valor,
+    data: req.body.data,
+    status: req.body.status,
+  });
   console.log(tabelaGasto);
   res.redirect("/financas");
 });
 
-exports.routes = router;
-exports.gasto = tabelaGasto;
+router.post("/add_ganho", (req, res, next) => {
+  tabelaGanho.push({
+    nome: req.body.nome,
+    valor: req.body.valor,
+    data: req.body.data,
+    origem: req.body.origem,
+  });
+  console.log("1", tabelaGanho, tabelaGasto);
+  res.redirect("/financas");
+});
+module.exports = router;
